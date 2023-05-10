d5c0bee1d4cdb0877da4bb153a43ef534f168eb3
Timxia77 <timxia@cmail.carleton.ca>
2023-05-10 14:26:53 -0400

    cache test

--- Cache Test #1/index.js
+++ Cache Test #1/index.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 const NodeCache = require("node-cache");
 
-const myCache = new NodeCache({stdTTL: 5}); //cache is 
+const myCache = new NodeCache({stdTTL: 5}); //cache is cleared after 5 seconds
 
 const app = express();
 const PORT = 3000;
@@ -12,7 +12,21 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+app.get("/textfile", (req, res) => {
+    // if (myCache.has("textfile")){
+    //     console.log("getting from cache");
+    //     return res.send(myCache.get("textfile"));
+    // } else {
+    //     console.log("getting from server");
+    //     let filePath = __dirname + "/index.html";
+    //     myCache.set("textfile", __dirname + "/index.html");
+    //     res.sendFile(filePath);
+    // }
+
+    res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(PORT, () => {
-    console.log(`Cache Test #1 is running on port ${PORT}.`)
-    console.log(`Test this at ${"http://localhost:3000"}`)
+    console.log(`Cache Test #1 is running on port ${PORT}.`);
+    console.log(`Test this at ${"http://localhost:3000"}`);
 });
